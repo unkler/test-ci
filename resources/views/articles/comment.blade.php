ce2f76f9767580152ae3612fb9de974b072e58bd
unkler <poru@add-natural.com>
2022-09-03 20:49:34 +0900

    コメント機能の改善 #25 対応、不必要なコメントを削除

--- resources/views/articles/comment.blade.php
+++ resources/views/articles/comment.blade.php
@@ -1,10 +1,17 @@
 @foreach ($article->comments as $comment)
 <div class="card">
-    <div class="card-header">
-      コメント
+  @if($loop->first)
+  <div class="card-header">
+    コメント
+  </div>
+  @endif
+  <div class="card-body">
+    <div class="small font-weight-lighter">
+      {{ $comment->created_at->format('Y/m/d H:i') }}
     </div>
-    <div class="card-body">
-      <p class="card-text">{{ $comment->body }}</p>
+    <div class="card-text">
+      {{ $comment->body }}
     </div>
   </div>
+</div>
 @endforeach
