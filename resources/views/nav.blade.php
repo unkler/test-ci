8028aeb481ffca7a5f661c99147a2cec6dfd0f2c
unkler <poru@add-natural.com>
2022-09-10 01:11:21 +0900

    アプリ名の変更 #37 対応

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand navbar-dark blue-gradient">
 
-  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>memo</a>
+  <a class="navbar-brand" href="/"><i class="fas fa-comment-dots mr-1"></i>Share Info</a>
 
   <ul class="navbar-nav ml-auto">
 
